97f4fe5c4b81d0b9cf841541a072e8a17e59427f
= <=>
2025-07-24 17:42:17 +0200

    clean

--- src/dbt_dataplatform/models/product/spotify/product_spotify__daily_artist_counts.sql
+++
@@ -1,8 +0,0 @@
-{{ config(dataset=get_schema('product'), materialized='view') }}
-SELECT
-    DATE(played_at) AS play_date,
-    artist_id,
-    artist_name,
-    COUNT(*) AS play_count
-FROM {{ ref('hub_spotify__plays') }}
-GROUP BY 1, 2, 3
