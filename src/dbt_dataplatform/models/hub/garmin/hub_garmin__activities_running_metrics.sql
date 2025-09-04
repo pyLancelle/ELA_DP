1238e510f586784145784f1b9c57c313a4e82da8
pyLancelle <etiennelancelle@outlook.fr>
2025-09-04 14:47:54 +0200

    feat: add new lake / hub tables / views

---
+++ src/dbt_dataplatform/models/hub/garmin/hub_garmin__activities_running_metrics.sql
@@ -0,0 +1,107 @@
+{{ config(dataset=get_schema('hub'), materialized='view', tags=["hub", "garmin"]) }}
+
+SELECT 
+  -- Identifiants source
+  JSON_EXTRACT_SCALAR(raw_data, '$.activityId') as activity_id,
+  
+  -- Timestamps source
+  TIMESTAMP(JSON_EXTRACT_SCALAR(raw_data, '$.startTimeGMT')) as start_time_utc,
+  TIMESTAMP(JSON_EXTRACT_SCALAR(raw_data, '$.startTimeLocal')) as start_time_local,
+  
+  -- Métadonnées activité
+  JSON_EXTRACT_SCALAR(raw_data, '$.activityName') as activity_name,
+  JSON_EXTRACT_SCALAR(raw_data, '$.activityType.typeKey') as activity_type,
+  CAST(JSON_EXTRACT_SCALAR(raw_data, '$.sportTypeId') AS INT64) as sport_type_id,
+  JSON_EXTRACT_SCALAR(raw_data, '$.manufacturer') as manufacturer,
+  
+  -- Struct durées (valeurs brutes secondes)
+  STRUCT(
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.duration') AS FLOAT64) as duration_seconds,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.elapsedDuration') AS FLOAT64) as elapsed_duration_seconds,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.movingDuration') AS FLOAT64) as moving_duration_seconds
+  ) as duration_raw,
+  
+  -- Struct distance et vitesse (valeurs brutes sources)
+  STRUCT(
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.distance') AS FLOAT64) as distance_meters,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.averageSpeed') AS FLOAT64) as average_speed,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.maxSpeed') AS FLOAT64) as max_speed,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.minActivityLapDuration') AS FLOAT64) as min_activity_lap_duration
+  ) as distance_speed_raw,
+  
+  -- Struct fréquence cardiaque (valeurs brutes sources)
+  STRUCT(
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.averageHR') AS INT64) as average_hr,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.maxHR') AS INT64) as max_hr,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.timeInHRZone1') AS FLOAT64) as hr_zone_1_time_s,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.timeInHRZone2') AS FLOAT64) as hr_zone_2_time_s,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.timeInHRZone3') AS FLOAT64) as hr_zone_3_time_s,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.timeInHRZone4') AS FLOAT64) as hr_zone_4_time_s,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.timeInHRZone5') AS FLOAT64) as hr_zone_5_time_s
+  ) as heart_rate_raw,
+  
+  -- Struct running spécifique (valeurs brutes sources)
+  STRUCT(
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.averageRunningCadenceInStepsPerMinute') AS FLOAT64) as avg_cadence_spm,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.maxRunningCadenceInStepsPerMinute') AS FLOAT64) as max_cadence_spm,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.steps') AS INT64) as steps,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.averageStrideLength') AS FLOAT64) as average_stride_length,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.averageVerticalOscillation') AS FLOAT64) as average_vertical_oscillation,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.averageVerticalRatio') AS FLOAT64) as average_vertical_ratio,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.averageGroundContactTime') AS FLOAT64) as average_ground_contact_time
+  ) as running_biomechanics_raw,
+  
+  -- Struct entraînement (valeurs brutes sources)
+  STRUCT(
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.trainingLoad') AS FLOAT64) as training_load,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.trainingStressScore') AS FLOAT64) as training_stress_score,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.aerobicTrainingEffect') AS FLOAT64) as aerobic_training_effect,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.anaerobicTrainingEffect') AS FLOAT64) as anaerobic_training_effect,
+    JSON_EXTRACT_SCALAR(raw_data, '$.trainingEffectLabel') as training_effect_label
+  ) as training_metrics_raw,
+  
+  -- Struct calories et énergie (valeurs brutes sources)
+  STRUCT(
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.calories') AS FLOAT64) as calories,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.bmrCalories') AS FLOAT64) as bmr_calories,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.differenceBodyBattery') AS INT64) as difference_body_battery,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.waterConsumed') AS FLOAT64) as water_consumed,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.waterEstimated') AS FLOAT64) as water_estimated
+  ) as energy_raw,
+  
+  -- Struct intensité (valeurs brutes sources) 
+  STRUCT(
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.moderateIntensityMinutes') AS FLOAT64) as moderate_intensity_minutes,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.vigorousIntensityMinutes') AS FLOAT64) as vigorous_intensity_minutes
+  ) as intensity_raw,
+  
+  -- Struct puissance (valeurs brutes sources)
+  STRUCT(
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.averagePower') AS FLOAT64) as average_power,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.maxPower') AS FLOAT64) as max_power,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.normPower') AS FLOAT64) as norm_power,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.timeInPowerZone1') AS FLOAT64) as power_zone_1_time_s,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.timeInPowerZone2') AS FLOAT64) as power_zone_2_time_s,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.timeInPowerZone3') AS FLOAT64) as power_zone_3_time_s,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.timeInPowerZone4') AS FLOAT64) as power_zone_4_time_s,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.timeInPowerZone5') AS FLOAT64) as power_zone_5_time_s
+  ) as power_raw,
+  
+  -- Struct environnemental (si disponible)
+  STRUCT(
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.temperature') AS FLOAT64) as temperature
+  ) as environmental_raw,
+  
+  -- Métadonnées techniques
+  STRUCT(
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.lapCount') AS INT64) as lap_count,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.hasHeartRateData') AS BOOL) as has_heart_rate_data,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.hasPolyline') AS BOOL) as has_polyline,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.hasSplits') AS BOOL) as has_splits,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.elevationCorrected') AS BOOL) as elevation_corrected,
+    CAST(JSON_EXTRACT_SCALAR(raw_data, '$.manualActivity') AS BOOL) as manual_activity
+  ) as technical_metadata,
+  
+  dp_inserted_at
+FROM {{ ref('lake_garmin__svc_activity_details') }}
+WHERE JSON_EXTRACT_SCALAR(raw_data, '$.activityType.typeKey') like '%running%'
---
+++ src/dbt_dataplatform/models/hub/garmin/hub_garmin__activities_running_segments.sql
@@ -0,0 +1,103 @@
+# ne marche pas, il faut utiliser ce qui est stocké dans raw_data
+{{ config(
+  materialized='table', 
+  partition_by={'field': 'activity_date', 'data_type': 'date'},
+  cluster_by=['user_id', 'activity_id'],
+  dataset=get_schema('hub'),
+  tags=["hub", "garmin"]) 
+}}
+
+WITH timeseries_extract AS (
+  SELECT 
+    activity_id,
+    user_id,
+    device_id,
+    start_time_utc,
+    DATE(start_time_utc) as activity_date,
+    
+    -- Point temporel dans la séquence
+    metric_point_index,
+    
+    -- Extraction des 19 métriques par point depuis detailed_data
+    -- Chaque point contient un array de 19 valeurs selon metricDescriptors
+    CAST(JSON_EXTRACT_SCALAR(metric_point, '$[0]') AS FLOAT64) as direct_vertical_oscillation_cm,
+    CAST(JSON_EXTRACT_SCALAR(metric_point, '$[1]') AS FLOAT64) as direct_available_stamina,
+    CAST(JSON_EXTRACT_SCALAR(metric_point, '$[2]') AS FLOAT64) as direct_fractional_cadence,
+    CAST(JSON_EXTRACT_SCALAR(metric_point, '$[3]') AS FLOAT64) as direct_double_cadence,
+    CAST(JSON_EXTRACT_SCALAR(metric_point, '$[4]') AS FLOAT64) as direct_body_battery,
+    CAST(JSON_EXTRACT_SCALAR(metric_point, '$[5]') AS FLOAT64) as sum_moving_duration_ms,
+    CAST(JSON_EXTRACT_SCALAR(metric_point, '$[6]') AS FLOAT64) as direct_run_cadence,
+    CAST(JSON_EXTRACT_SCALAR(metric_point, '$[7]') AS FLOAT64) as sum_accumulated_power_watts,
+    CAST(JSON_EXTRACT_SCALAR(metric_point, '$[8]') AS FLOAT64) as sum_distance_cm,
+    CAST(JSON_EXTRACT_SCALAR(metric_point, '$[9]') AS FLOAT64) as direct_potential_stamina,
+    CAST(JSON_EXTRACT_SCALAR(metric_point, '$[10]') AS INT64) as direct_timestamp_gmt,
+    CAST(JSON_EXTRACT_SCALAR(metric_point, '$[11]') AS FLOAT64) as direct_speed_mps_x10,
+    CAST(JSON_EXTRACT_SCALAR(metric_point, '$[12]') AS FLOAT64) as sum_elapsed_duration_ms,
+    CAST(JSON_EXTRACT_SCALAR(metric_point, '$[13]') AS FLOAT64) as direct_power_watts,
+    CAST(JSON_EXTRACT_SCALAR(metric_point, '$[14]') AS FLOAT64) as direct_heart_rate_bpm,
+    CAST(JSON_EXTRACT_SCALAR(metric_point, '$[15]') AS FLOAT64) as sum_duration_ms,
+    CAST(JSON_EXTRACT_SCALAR(metric_point, '$[16]') AS FLOAT64) as direct_stride_length_cm,
+    CAST(JSON_EXTRACT_SCALAR(metric_point, '$[17]') AS FLOAT64) as direct_vertical_ratio,
+    CAST(JSON_EXTRACT_SCALAR(metric_point, '$[18]') AS FLOAT64) as direct_ground_contact_time_ms,
+    
+    loaded_at
+
+  FROM {{ ref('lake_garmin__svc_activity_details') }},
+  UNNEST(JSON_EXTRACT_ARRAY(detailed_data, '$.activityDetailMetrics')) 
+    WITH OFFSET AS metric_point_index
+  WHERE activity_type = 'running'
+    AND detailed_data IS NOT NULL
+    AND JSON_EXTRACT_ARRAY(detailed_data, '$.activityDetailMetrics') IS NOT NULL
+)
+
+SELECT 
+  activity_id,
+  user_id,
+  device_id,
+  activity_date,
+  start_time_utc,
+  metric_point_index,
+  
+  -- Struct temporel (timestamps et durées brutes)
+  STRUCT(
+    direct_timestamp_gmt,
+    sum_moving_duration_ms,
+    sum_elapsed_duration_ms,
+    sum_duration_ms
+  ) as temporal_raw,
+  
+  -- Struct physiologique (valeurs brutes capteurs)
+  STRUCT(
+    direct_heart_rate_bpm,
+    direct_body_battery,
+    direct_available_stamina,
+    direct_potential_stamina
+  ) as physiological_raw,
+  
+  -- Struct biomécanique running (valeurs brutes capteurs)
+  STRUCT(
+    direct_run_cadence as cadence_spm,
+    direct_fractional_cadence,
+    direct_double_cadence,
+    direct_stride_length_cm,
+    direct_vertical_oscillation_cm,
+    direct_vertical_ratio,
+    direct_ground_contact_time_ms
+  ) as biomechanical_raw,
+  
+  -- Struct vitesse et distance (valeurs brutes capteurs avec facteurs)
+  STRUCT(
+    direct_speed_mps_x10, -- Facteur 0.1 selon metricDescriptors
+    sum_distance_cm,      -- Facteur 100 selon metricDescriptors  
+    sum_accumulated_power_watts
+  ) as performance_raw,
+  
+  -- Struct puissance (valeurs brutes capteurs)
+  STRUCT(
+    direct_power_watts,
+    sum_accumulated_power_watts
+  ) as power_raw,
+  
+  dp_inserted_at
+
+FROM timeseries_extract;
