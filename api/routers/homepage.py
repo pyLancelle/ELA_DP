dc2089b7c524d1ba242878a09d443f32df44dc4b
= <etiennelancelle@outlook.fr>
2025-12-27 00:49:53 +0100

    fix

--- api/routers/homepage.py
+++ api/routers/homepage.py
@@ -43,13 +43,7 @@ async def get_music_time_daily():
 async def get_race_prediction():
     """Récupère les prédictions de temps de course pour différentes distances"""
     query = f"""
-        SELECT
-            distance,
-            current_date,
-            current_time,
-            previous_date,
-            previous_time,
-            diff_seconds
+        SELECT *
         FROM `{PROJECT_ID}.{DATASET}.pct_homepage__race_prediction`
         ORDER BY CASE distance
             WHEN '5K' THEN 1
